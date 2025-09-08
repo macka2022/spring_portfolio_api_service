df9be58e603ffd55dc8f092b6866b859397f0a0f
macka2022 <amadoumackadiallo375@gmail.com>
2025-09-08 08:00:12 +0000

    fiing error db 1

--- src/test/java/com/example/demoportflio/DemoportflioApplicationTests.java
+++ src/test/java/com/example/demoportflio/DemoportflioApplicationTests.java
@@ -4,9 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
-
+@ActiveProfiles("test")
 @SpringBootTest
 @TestPropertySource(locations = "classpath:application-test.yml")
 class DemoportflioApplicationTests {
