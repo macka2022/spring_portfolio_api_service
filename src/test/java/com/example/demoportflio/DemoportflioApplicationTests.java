ec0ccbf3159ae6ea2a7caecb652acda0d72d567d
macka2022 <amadoumackadiallo375@gmail.com>
2025-09-08 08:35:28 +0000

    fiing error db 1

--- src/test/java/com/example/demoportflio/DemoportflioApplicationTests.java
+++ src/test/java/com/example/demoportflio/DemoportflioApplicationTests.java
@@ -4,11 +4,8 @@ import org.junit.jupiter.api.Test;
 
 import org.springframework.boot.test.context.SpringBootTest;
 
-import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.context.TestPropertySource;
 
-//@ActiveProfiles("test")
-@ActiveProfiles("test")  // ← Utilisez cette annotation à la place
+
 @SpringBootTest
 class DemoportflioApplicationTests {
 
